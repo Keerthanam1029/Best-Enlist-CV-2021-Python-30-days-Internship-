7fefc52a978c94f408c6710e5f2cc6dd66481737
Keerthana Mahalingam <68453072+Keerthanam1029@users.noreply.github.com>
2021-06-15 17:46:35 +0530

    Task 4 assignments
    Assignments related to data types and Variables

---
+++ Task4.py
@@ -0,0 +1,33 @@
+#Assigning value
+a=b=c=5000
+print("Values of a=",a,"b=",b,"and c=",c)
+
+#a divide by 10
+print("Divide a by 10 ",a/10)
+
+
+#b multiply by 50
+print("Multiply b by 50",b*50)
+
+#Add c value by 60
+print("Add c value by 60",c+60)
+
+#-------------------------------------------------------------------------------------------
+#Q2. Create a string variable of 5 character and replace 3rd character with G
+
+#Creating string of 5 variables
+string = "aeiou"
+print(string)
+
+#replacing "0" with "G"
+print("Replacing G in string = ",string.replace("0","G"))
+
+#---------------------------------------------------------------------------------------------
+#Q3. Create two values (a,b) of int,float data type & convert the vise versa,
+
+#Creating values(a,b) of int, float
+a=50
+b=50.74
+
+print("Int to Float a: ",float(a))
+print("Float to Int b: ",int(b))
